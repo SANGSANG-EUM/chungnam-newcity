3d6197810916ea7ae80025f9e2215ad122da288e
onesick <onesick@iium.kr>
2023-11-06 18:06:05 +0900

    메인-개발특성화계획 작업중
    -상단 지도 및 조성현황 마크업 완료

--- sources/js/normal.js
+++ sources/js/normal.js
@@ -5,5 +5,18 @@ function printPage() {
 
 // Document Ready Function
 $(document).ready(function(){
-  
+  var swiper = new Swiper(".main_plan-slider .swiper-container", {
+    slidesPerView: 5,
+    spaceBetween: 50,
+    centeredSlides: true,
+    loop: true,
+    // pagination: {
+    //   el: ".swiper-pagination",
+    //   clickable: true,
+    // },
+    // navigation: {
+    //   nextEl: ".swiper-button-next",
+    //   prevEl: ".swiper-button-prev",
+    // },
+  });
 });
